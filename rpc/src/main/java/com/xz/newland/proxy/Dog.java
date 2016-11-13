a359690377ec42cde55e99e3c4f3948837e02420
pop443 <xzparty@sina.com>
2016-11-13 15:01:24 +0800

    nio

---
+++ rpc/src/main/java/com/xz/newland/proxy/Dog.java
@@ -0,0 +1,12 @@
+package com.xz.newland.proxy;
+
+/**
+ * falcon -- 2016/11/13.
+ */
+class Dog implements Animal{
+
+    @Override
+    public String jiao(String s ) {
+        return "dog 旺:"+s;
+    }
+}
