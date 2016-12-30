12df3c2a91aaf7bb4f0ef29455224445224336d6
pop443 <xzparty@sina.com>
2016-12-30 16:10:35 +0800

    http

---
+++ other/src/main/java/com/xz/other/mail/Email.java
@@ -0,0 +1,68 @@
+package com.xz.other.mail;
+
+
+import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * falcon -- 2016/12/30.
+ */
+public class Email {
+    public static void main(String[] args) throws MessagingException {
+    // 配置发送邮件的环境属性
+    final Properties props = new Properties();
+
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.host", "smtp.sina.com");
+    // 发件人的账号
+    props.put("mail.user", "xzparty@sina.com");
+    // 访问SMTP服务时需要提供的密码
+    props.put("mail.password", "5584143xz");
+
+    // 构建授权信息，用于进行SMTP进行身份验证
+    Authenticator authenticator = new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+            // 用户名、密码
+            String userName = props.getProperty("mail.user");
+            String password = props.getProperty("mail.password");
+            return new PasswordAuthentication(userName, password);
+        }
+    };
+    // 使用环境属性和授权信息，创建邮件会话
+    Session mailSession = Session.getInstance(props, authenticator);
+    // 创建邮件消息
+    MimeMessage message = new MimeMessage(mailSession);
+    // 设置发件人
+    InternetAddress form = new InternetAddress(props.getProperty("mail.user"));
+    message.setFrom(form);
+
+    // 设置收件人
+    InternetAddress to = new InternetAddress("13675110430@139.com");
+    message.setRecipient(RecipientType.TO, to);
+
+    // 设置抄送
+    InternetAddress cc = new InternetAddress("236981120@qq.com");
+    message.setRecipient(RecipientType.CC, cc);
+
+    // 设置密送，其他的收件人不能看到密送的邮件地址
+    InternetAddress bcc = new InternetAddress("xzparty@sina.com");
+    message.setRecipient(RecipientType.BCC, bcc);
+
+    // 设置邮件标题
+    message.setSubject("测试邮件");
+
+    // 设置邮件的内容体
+    message.setContent("hello", "text/html;charset=UTF-8");
+
+    // 发送邮件
+    Transport.send(message);
+}
+}
