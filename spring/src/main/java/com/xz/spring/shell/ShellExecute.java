f7c951273d4d4ee8d9efb3b2a1aec63f354e14c1
pop443 <xzparty@sina.com>
2017-01-05 00:01:12 +0800

    jline

--- spring/src/main/java/com/xz/spring/shell/ShellExecute.java
+++ spring/src/main/java/com/xz/spring/shell/ShellExecute.java
@@ -1,9 +1,13 @@
 package com.xz.spring.shell;
 
 import jline.console.ConsoleReader;
+import jline.console.completer.*;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * falcon -- 2017/1/4.
@@ -12,10 +16,31 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackageClasses = {ShellExecute.class})
 public class ShellExecute {
 
-    public void run(String... strings) throws Exception {
+    public void run(String... jline) throws Exception {
         ConsoleReader reader = new ConsoleReader() ;
+
+        List<Completer> completors = new ArrayList<>();
+        completors.add(new FileNameCompleter());
+        completors.add(new StringsCompleter("echo","fuck"));
+        completors.add(new NullCompleter()) ;
+        completors.add(new ArgumentCompleter()) ;
+        reader.addCompleter(new ArgumentCompleter(completors)) ;
+
         reader.setExpandEvents(false);
         reader.setBellEnabled(false);
+        //Command History
+        String historyPath = System.getProperty("") ;
+        reader.setHistoryEnabled(true);
+        String line = null;
+        do
+        {
+            line = reader.readLine("input>");
+            if(line != null)
+            {
+                System.out.println(line);
+            }
+        }
+        while(/*line!=null && */!line.equals("exist")) ;
     }
 
     public static void main(String[] args) {
