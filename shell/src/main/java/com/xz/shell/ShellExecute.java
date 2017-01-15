a0db02f1d6b3dccbac748327ce9901b51f35e257
pop443 <xzparty@sina.com>
2017-01-15 21:14:15 +0800

    jline

---
+++ shell/src/main/java/com/xz/shell/ShellExecute.java
@@ -0,0 +1,45 @@
+package com.xz.shell;
+
+import jline.console.ConsoleReader;
+import jline.console.history.FileHistory;
+import jline.console.history.History;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * falcon -- 2017/1/4.
+ */
+@Configuration
+@ComponentScan(basePackageClasses = {ShellExecute.class})
+public class ShellExecute {
+    private String historyFile = ".xzShellConf";
+
+    public void run(String... strings) throws Exception {
+        ConsoleReader reader = new ConsoleReader();
+        //history
+        reader.setHistoryEnabled(true);
+        File fileHistory = new File(System.getProperty("user.home") + File.separator + historyFile);
+        History history = new FileHistory(fileHistory);
+        reader.setHistory(history);
+
+
+        reader.setExpandEvents(false);
+        reader.setBellEnabled(false);
+
+    }
+
+    public static void main(String[] args) {
+        for (Map.Entry entry : System.getProperties().entrySet()) {
+            String key = (String) entry.getKey();
+            String value = (String) entry.getValue();
+            System.out.println(key + "--" + value);
+        }
+
+
+        new SpringApplicationBuilder(ShellExecute.class).showBanner(false).run(args);
+    }
+}
