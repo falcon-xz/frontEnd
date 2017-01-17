5651c3b2e85307d212086a97c5da6b74d27263fc
pop443 <xzparty@sina.com>
2017-01-17 23:27:30 +0800

    jline

--- shell/src/main/java/com/xz/shell/ShellExecute.java
+++ shell/src/main/java/com/xz/shell/ShellExecute.java
@@ -1,24 +1,27 @@
 package com.xz.shell;
 
 import jline.console.ConsoleReader;
+import jline.console.completer.Completer;
+import jline.console.completer.StringsCompleter;
 import jline.console.history.FileHistory;
 import jline.console.history.History;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * falcon -- 2017/1/4.
  */
-@Configuration
-@ComponentScan(basePackageClasses = {ShellExecute.class})
 public class ShellExecute {
-    private String historyFile = ".xzShellConf";
 
-    public void run(String... strings) throws Exception {
+    private String historyFile = null;
+
+    public ShellExecute() {
+        this.historyFile = ".xzShellConf";
+    }
+
+    public void run(String[] args) throws Exception {
         ConsoleReader reader = new ConsoleReader();
         //history {use_home}/{historyFile}
         reader.setHistoryEnabled(true);
@@ -26,21 +29,37 @@ public class ShellExecute {
         History history = new FileHistory(fileHistory);
         reader.setHistory(history);
 
+        List<String> list = new ArrayList<>() ;
+        list.add("hello");
+        list.add("fuck");
+        list.add("fuck1");
+        Completer completer = new StringsCompleter(list) ;
+        reader.addCompleter(completer) ;
 
         reader.setExpandEvents(false);
         reader.setBellEnabled(false);
 
 
+        String prefix = "";
+        String line;
+        String tip = "xz" ;
+        while ((line = reader.readLine(tip+"> ")) != null) {
+            if (!prefix.equals("")) {
+                prefix = prefix+'\n';
+            }
+            System.out.println("step3:"+line.trim().endsWith(";")+!line.trim().endsWith("\\;"));
+            if ((line.trim().endsWith(";")) && (!line.trim().endsWith("\\;"))) {
+                line = prefix+line;
+                System.out.println(line);
+                prefix = "" ;
+            } else {
+                prefix = prefix+line;
+            }
+
+        }
     }
 
-    public static void main(String[] args) {
-        for (Map.Entry entry : System.getProperties().entrySet()) {
-            String key = (String) entry.getKey();
-            String value = (String) entry.getValue();
-            System.out.println(key + "--" + value);
-        }
-
-
-        new SpringApplicationBuilder(ShellExecute.class).showBanner(false).run(args);
+    public static void main(String[] args) throws Exception {
+        new ShellExecute().run(args);
     }
 }
