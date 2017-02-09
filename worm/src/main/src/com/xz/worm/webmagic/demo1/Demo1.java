27926cb6046e3f9dca607016da208fd9db085b99
pop443 <xzparty@sina.com>
2017-02-09 15:58:35 +0800

    webmagic

--- worm/src/main/src/com/xz/worm/webmagic/demo1/Demo1.java
+++ worm/src/main/src/com/xz/worm/webmagic/demo1/Demo1.java
@@ -2,14 +2,15 @@ package com.xz.worm.webmagic.demo1;
 
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.SimplePageProcessor;
+import us.codecraft.webmagic.processor.example.GithubRepoPageProcessor;
 
 /**
  * falcon -- 2017/2/9.
  */
 public class Demo1 {
     public static void main(String[] args) {
-        Spider.create(new SimplePageProcessor("http://my.oschina.net/",
-                "http://my.oschina.net/*/blog/*")).thread(2).run();
+        Spider.create(new SimplePageProcessor("http://my.oschina.net/","http://my.oschina.net*//*//*blog*//*")).thread(2).run();
+        //Spider.create(new GithubRepoPageProcessor()).addUrl(new String[]{"https://github.com/code4craft"}).thread(2).run();
     }
 
 }
