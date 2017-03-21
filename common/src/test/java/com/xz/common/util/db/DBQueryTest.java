c787b1281ca6f03d5a59f85b3505964f02878998
pop443 <xzparty@sina.com>
2017-03-21 21:02:54 +0800

    spring 隔离

--- common/src/test/java/com/xz/common/util/db/DBQueryTest.java
+++ common/src/test/java/com/xz/common/util/db/DBQueryTest.java
@@ -10,7 +10,7 @@ import java.sql.*;
 public class DBQueryTest {
     public static void main(String[] args) {
         try {
-            Connection conn = DBUtil.getConnection("ambari");
+            Connection conn = DBUtil.getConnection("hive");
             PreparedStatement pstm = conn.prepareStatement("select ? from dual") ;
             pstm.setString(1,"2");
             ResultSet rs = pstm.executeQuery() ;
