feb928a554fb4f29a9338ea3048ac4deb35dbfcf
pop443 <xzparty@sina.com>
2017-07-19 20:21:01 +0800

    singleton

--- designMode/src/main/java/com/xz/designMode/createType/singletion/Singleton4.java
+++ designMode/src/main/java/com/xz/designMode/createType/singletion/Singleton4.java
@@ -12,4 +12,12 @@ public class Singleton4 {
 	private static class Singleton4Holder{
 		private static Singleton4 singleton4 = new Singleton4() ;
 	}
+
+	public static void main(String[] args) {
+		Singleton4 singleton1 = Singleton4.getInstance() ;
+		Singleton4 singleton2 = Singleton4.getInstance() ;
+		Singleton4 singleton3 = Singleton4.getInstance() ;
+		System.out.println(singleton1==singleton2);
+		System.out.println(singleton3==singleton2);
+	}
 }
