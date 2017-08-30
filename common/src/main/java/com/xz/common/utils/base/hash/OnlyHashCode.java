e64199c791323b86fb5aa713d09a549c5846a056
pop443 <xzparty@sina.com>
2017-08-30 16:34:07 +0800

    hashcode

---
+++ common/src/main/java/com/xz/common/utils/base/hash/OnlyHashCode.java
@@ -0,0 +1,83 @@
+package com.xz.common.utils.base.hash;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ ************ 相等的对象必须有相等的hashcode  ************
+ ************ hashcode不等   equals一定不等   指未重写的equals ************
+ ************ hashcode相等   equals不一定相等 ************
+ * hashcode是用于散列数据的快速存取，
+ * 如利用HashSet/HashMap/Hashtable类来存储数据时，
+ * 都是根据存储对象的hashcode值来进行判断是否相同的
+ * 此例 hashcode相等   重写equals相等  认定为相同user对象
+ */
+public class OnlyHashCode {
+
+    public static void main(String[] args) {
+        Collection<User> c = new HashSet<>();
+        User u1 = new User("xz",12);
+        User u2 = new User("xz",12);
+        User u3 = new User("xz",12);
+        c.add(u1);
+        c.add(u2);
+        c.add(u3);
+        for (User u:c) {
+            System.out.println(u);
+        }
+    }
+
+
+    static class User{
+        public User(String name, int ager) {
+            this.name = name;
+            this.ager = ager;
+        }
+
+        private String name ;
+        private int ager ;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAger() {
+            return ager;
+        }
+
+        public void setAger(int ager) {
+            this.ager = ager;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", ager=" + ager +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User user = (User) o;
+
+            if (ager != user.ager) return false;
+            return name.equals(user.name);
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            result = 31 * result + ager;
+            return result;
+        }
+    }
+}
--- io/src/test/java/com/xz/io/baseio/ReadLineTest.java
+++ io/src/test/java/com/xz/io/baseio/ReadLineTest.java
@@ -9,63 +9,33 @@ import java.io.LineNumberReader;
  * falcon -- 2017/3/15.
  */
 public class ReadLineTest {
-    public static int i = 0 ;
+    public static int i = 0;
+
     public static void main(String[] args) {
-        File file = new File("E:\\1.txt") ;
-        try {
-            FileReader fr = new FileReader(file);
-            LineNumberReader lnr = new LineNumberReader(fr) ;
-            String line = null ;
-            StringBuilder sb = new StringBuilder();
-            while ((line = lnr.readLine())!=null){
-                ReadLineTest.config2(line,sb);
+        File pfile = new File("C:\\Users\\Administrator\\Desktop\\1\\spark_log");
+        File[] files1 = pfile.listFiles();
+        for (File files:files1) {
+            File[] files2 = files.listFiles();
+            for (File file : files2) {
+                try {
+                    FileReader fr = new FileReader(file);
+                    LineNumberReader lnr = new LineNumberReader(fr);
+                    String line = null;
+                    StringBuilder sb = new StringBuilder();
+                    while ((line = lnr.readLine()) != null) {
+                        if (line.contains("Exception")){
+                            System.out.println(line);
+                            System.out.println(file.getName());
+                        }
+                    }
+                    lnr.close();
+                    fr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            lnr.close();
-            fr.close();
-            System.out.println(sb.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-
-    public static void config1(String line,StringBuilder sb){
-        if (line.split("\\t").length==0){
-            return ;
-        }
-        String key = line.split("\\t")[0] ;
-        String value = line.split("\\t")[1] ;
-        if (value.toLowerCase().equals("true") || value.toLowerCase().equals("false")){
-            value = value.toLowerCase() ;
-        }
-        sb.append("<property>").append("\n");
-        sb.append("<name>").append(key).append("</name>").append("\n");
-        sb.append("<value>").append(value).append("</value>").append("\n");
-        sb.append("<display-name>").append(key).append("</display-name>").append("\n");
-        if (value.equals("true") || value.equals("false")){
-            sb.append("<value-attributes>").append("\n");
-            sb.append(" <type>boolean</type>").append("\n");
-            sb.append(" <overridable>false</overridable>").append("\n");;
-            sb.append("</value-attributes>").append("\n");
-        }
-        sb.append("</property>").append("\n");
-    }
-
-    public static void config2(String line,StringBuilder sb){
-        if (line.split("\\t").length==0){
-            return ;
-        }
-        String key = line.split("\\t")[0] ;
-        String value = line.split("\\t")[1] ;
-        if (value.toLowerCase().equals("true") || value.toLowerCase().equals("false")){
-            value = value.toLowerCase() ;
-        }
-        sb.append("{").append("\n");
-        sb.append("\"name\": \"").append(key).append("\",").append("\n");
-        sb.append("\"serviceName\": \"JSTORM\",").append("\n");
-        sb.append("\"filename\": \"jstorm-env.xml\",").append("\n");
-        sb.append("\"category\": \"DRPC_SERVER\",").append("\n");
-        sb.append("\"index\": \"").append(i++).append("\n");
-        sb.append("},").append("\n");
 
     }
+
 }
